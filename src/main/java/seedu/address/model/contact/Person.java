cb163a6fde3e90d484234a371b6a8cc2b131ddf8
Reiner Ong <e1337575@u.nus.edu>
2026-03-11 17:25:33 +0800

    Refactor Person to extend Contact abstract class

---
+++ src/main/java/seedu/address/model/contact/Person.java
@@ -0,0 +1,33 @@
+package seedu.address.model.contact;
+
+import java.util.Set;
+
+import seedu.address.model.tag.Tag;
+
+/**
+ * Represents a Person in the address book.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
+public class Person extends Contact {
+
+    /**
+     * Every field must be present and not null.
+     */
+    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
+        super(name, phone, email, address, tags);
+    }
+
+    /**
+     * Returns true if both persons have the same name.
+     * This defines a weaker notion of equality between two persons.
+     */
+    public boolean isSamePerson(Person otherPerson) {
+        if (otherPerson == this) {
+            return true;
+        }
+
+        return otherPerson != null
+                && otherPerson.getName().equals(getName());
+    }
+
+}
